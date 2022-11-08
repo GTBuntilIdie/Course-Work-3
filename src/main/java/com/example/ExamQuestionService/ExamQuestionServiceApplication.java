e555b7769e5f02522a69f9094c0da99f13ca8423
Tamerlan <ishmaevtamerlan@gmail.com>
2022-11-08 13:23:14 +0300

    Course work

---
+++ src/main/java/com/example/ExamQuestionService/ExamQuestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamQuestionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionServiceApplication.class, args);
+	}
+
+}
